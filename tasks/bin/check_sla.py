b828c221eaff0c482cc519990aebd89ff1c4ffd8
Jonathan Azoff <jon@beomni.com>
2019-09-14 23:10:32 -0700

    posting to twitter

--- tasks/bin/check_sla.py
+++ tasks/bin/check_sla.py
@@ -6,6 +6,7 @@ import json
 import sys
 import os
 import sqlite3
+import twitter
 
 logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z', level=logging.INFO)
 
@@ -14,23 +15,38 @@ logging.info(f'connecting to database {db_path}...')
 conn = sqlite3.connect(db_path)
 conn.row_factory=sqlite3.Row
 
-offset = sys.argv[2] if len(sys.argv) > 2 else '-6 days'
-sla = float(sys.argv[3] if len(sys.argv) > 3 else (100 * 1000000))
+secrets = None
+secrets_path = sys.argv[2] if len(sys.argv) > 2 else os.environ['SECRETS_PATH']
+with open(secrets_path) as secrets_file:
+	secrets = json.load(secrets_file)
+
+offset = secrets['offset']
 logging.info(f'checking {offset} average...')
 cur = conn.cursor()
 cur.execute('''
-	select avg(download) as `avg`
+	select download, share
 	from speedtest
 	where datetime(`timestamp`) >= datetime('now', ?)
+	order by download desc
 ''', (offset,))
 
-avg = cur.fetchone()['avg']
-if avg is None:
+sla = secrets['minimum']
+row = cur.fetchone()
+download = round(row['download'] / 1000000, 2)
+if download is None:
 	logging.warning(f'no data found in {offset}')
-elif sla > avg:
-	logging.error(f'sample aggregate of {avg} is less than {sla}')
+elif sla < download:
+	logging.info(f'sample max of {download} mb/s is acceptable.')
 else:
-	logging.info(f'sample aggregate of {avg} is greater than {sla}')
+	logging.warning(f'sample max of {download} mb/s is less than acceptable min of {sla} mb/s...')
+	logging.info('connecting to twitter...')
+	api = twitter.Api(**secrets['twitter_api'])
+	logging.info(f'posting status update...')
+	msg = api.PostUpdate(secrets['status'] % {
+		'offset': secrets['offset'][1:],
+		'download': download
+	}, media=row['share'])
+	print('>>>', msg)
 
 
 logging.info(f'cleaning up...')
